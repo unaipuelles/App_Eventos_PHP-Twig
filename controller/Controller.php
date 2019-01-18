6cb936a9c4fc14c0892ee284f5f45df2452f4f74
unaipuelles <unai-p@hotmail.com>
2019-01-18 20:45:57 +0100

    Añadidos base de Controladores

--- controller/Controller.php
+++ controller/Controller.php
@@ -20,12 +20,39 @@ abstract class controller
         $this->twig = new Twig_Environment($loader);
     }
 
-    public function cargarArchivos() {
+    public function cargarArchivos()
+    {
         require_once __DIR__ . '/../core/Connection.php';
         require_once __DIR__ . '/../vendor/autoload.php';
     }
 
-    public function twigView($page, $data) {
+    public function run($action=null, $id=null)
+    {
+        switch ($action)
+        {
+            case 'createForm':
+                $this->createForm();
+            break;
+            case 'create':
+                $this->create();
+            break;
+            case 'delete':
+                $this->delete($id);
+            break;
+            case 'details':
+                $this->details($id);
+            break;
+            case 'modify':
+                $this->modify($id);
+            break;
+            default:
+                $this->defaultCase();
+            break;
+        }
+    }
+
+    public function twigView($page, $data)
+    {
         echo $this->twig->render($page, $data);
     }
 }
---
+++ controller/EventoController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: unaipuelles
+ * Date: 18/01/2019
+ * Time: 20:40
+ */
+
+include_once __DIR__ .'/../controller/Controller.php';
+
+class EventoController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function cargarArchivos()
+    {
+        parent::cargarArchivos();
+        require_once __DIR__ . '/../model/Evento.php';
+    }
+
+    public function run($action = null, $id = null)
+    {
+        parent::run($action, $id);
+    }
+
+    public function twigView($page, $data)
+    {
+        parent::twigView($page, $data);
+    }
+}
